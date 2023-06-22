d59907cc3343ab8ebf83a756d59ce2ec85561777
Pham Van Thanh <88926531+thanhlucifer@users.noreply.github.com>
2023-06-22 17:57:43 +0700

    Initial commit

---
+++ EcommerceWebSpringBoot/src/main/java/lucifer/example/EcommerceWebSpringBoot/EcommerceWebSpringBootApplication.java
@@ -0,0 +1,13 @@
+package lucifer.example.EcommerceWebSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceWebSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceWebSpringBootApplication.class, args);
+	}
+
+}
---
+++ EcommerceWebSpringBoot/src/test/java/lucifer/example/EcommerceWebSpringBoot/EcommerceWebSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package lucifer.example.EcommerceWebSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceWebSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
